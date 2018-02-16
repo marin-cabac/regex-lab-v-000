57ed98d5217757f09683c1998a1de9a62f1e43f5
Marin Cabac <marin.cabac@gmail.com>
2018-02-16 19:42:30 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,5 +16,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-phone.scan(/\d/).count.eql?(10)
+  !!(phone.match(/\d{10}/))
+#phone.scan(/\d/).count.eql?(10)
 end
