83c73d6a684bdd9198baa68fc87c8cd3769557a6
Marin Cabac <marin.cabac@gmail.com>
2018-02-16 19:44:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,12 +9,12 @@ end
 def words_five_letters_long(text)
  text.scan(/\b\w{5}\b/)
 end
-
+##############################################
 def first_word_capitalized_and_ends_with_punctuation?(text)
 !!text.match(/^[A-Z].+\W$/)
 
 end
-
+##############################################
 def valid_phone_number?(phone)
 phone.scan(/\d/).count.eql?(10)
 end
