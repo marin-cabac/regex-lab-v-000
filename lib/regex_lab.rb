a187f3413871b64081df455ade1a4202be2d2f79
Marin Cabac <marin.cabac@gmail.com>
2018-02-16 15:40:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-!!w.match(/^[A-Z].+\W$/)
+!!text.match(/^[A-Z].+\W$/)
+
 end
 
 def valid_phone_number?(phone)
